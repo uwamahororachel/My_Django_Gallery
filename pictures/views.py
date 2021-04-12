54237839653540bc18e6bcd8016b84a4192b1ca9
Rachel Uwamahoro <racheluwamahoro55@gmail.com>
2021-04-12 15:40:16 +0200

    additiona configurations for the view file

--- pictures/views.py
+++ pictures/views.py
@@ -19,3 +19,23 @@ def picture(request,category_name,image_id):
         raise Http404()
     return render(request,"gallery.html",{'title':title,"image":image, "locations":locations, "image_category":image_category})
 
+def search_by_cat(request):
+    title = 'Search'
+    if 'image_category' in request.GET and request.GET['image_category']:
+        search_term = request.GET.get('image_category')
+        found_results = Image.objects.filter(image_category__name=search_term)
+        message = f"{search_term}"
+        print(search_term)
+        print(found_results)
+
+        return render(request, 'search.html',{'title':title,'search_term':search_term,'images': found_results, 'message': message})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search.html',{"message": message})
+
+def location_filter(request, image_location):
+    locations = Location.objects.all()
+    location = Location.get_location_id(image_location)
+    images = Image.filter_by_location(image_location)
+    title = f'{location} Photos'
+    return render(request, 'location.html', {'images':images, 'locations':locations, 'location':location})
